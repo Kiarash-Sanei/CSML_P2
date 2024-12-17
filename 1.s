c8f566fe55023b5b71ea5bbb4a00f6d395d3433c
Kiarash Sanei <kiarashsanei5@gmail.com>
2024-12-17 23:01:28 +0330

    Ok without the flag

--- 1.s
+++ 1.s
@@ -1,35 +1,40 @@
 global asm_main
-extern printf 
+extern printf
 extern scanf
+
 section .text
 asm_main:
-    sub rsp, 8 ; fix allignments
+    sub rsp, 8 ; Align stack to 16 bytes (required for function calls)
 
-    mov rdi, scanf_format ; scanf format
-    mov rsi, rsp ; load the number to top of stack
-    ; add rsp, 4 ; make the stack ready
-    ; mov rdx, rsp ; load the others to top of stack
-
+    ; Read input with scanf
+    mov rdi, scanf_format ; Address of scanf format string
+    mov rsi, rsp ; Address to store input
     call scanf
 
-    ; mov rdx, [rsp] ; load the others from stack
-    ; sub rsp, 4 ; make stack ready
-    mov rsi, [rsp] ; load the number from stack
-    mov rax, rsi ; put the number to other place
-    and rax, 1E000h ; get 14th to 17th bit
-    ; mov rbx, rsi ; put the number to other place
-    ; and rbx, 0x780000 ; get 20th to 23rd bit
-    ; mov rdi, printf_format ; setting the first argument
-    ; mov rcx, rax ; put the first number in place
-    ; add rcx, rbx ; adding the second number
-    ; mov rsi, rax ; setting the second argument
-    ; mov rdi, rbx ; setting the third argument
+    ; Load the input number
+    mov rax, [rsp] ; Load the input number into eax
 
-    ; call printf
+    ; Extract 14th-17th bits
+    mov rsi, rax ; Copy number to rsi
+    and rsi, 0x3C000 ; Mask 14th-17th bits (0x3C000 = 245760)
+    shr rsi, 14 ; Shift to get the value (14th bit = LSB)
 
-    add rsp, 8
-    ret
-    
+    ; Extract 20th-23rd bits
+    mov rdx, rax ; Copy number to rdx
+    and rdx, 0xF00000 ; Mask 20th-23rd bits (0xF00000 = 15728640)
+    shr rdx, 20 ; Shift to get the value (20th bit = LSB)
+
+    ; Addition
+    mov rcx, rsi ; Copy number to rcx
+    add rcx, rdx ; Adding rdx
+
+    ; Print the results
+    mov rdi, printf_format ; Address of printf format string
+    call printf 
+
+    add rsp, 8 ; Restore stack alignment
+    ret ; Return from main
+
 section .data
-scanf_format: db "%d", 0,
-printf_format: db "%d\n%d\n%d" , 10 , 0,
+scanf_format: db "%d", 0 ; Format string for scanf
+printf_format: db "%d", 10, "%d", 10, "%d", 10, 0 ; Format string for printf

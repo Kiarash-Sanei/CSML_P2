1b37ac9e073abd1e7049219e938361bda36c29fe
Kiarash Sanei <kiarashsanei5@gmail.com>
2024-12-23 21:28:07 +0330

    still not ok!

--- 2.s
+++ 2.s
@@ -4,23 +4,30 @@ extern scanf
 
 section .text
 asm_main:
-    sub rsp, 8 ; Align stack to 16 bytes (required for function calls)
+    sub rsp, 8 ; Align stack to 8 bytes (required for function calls)
 
     ; Read input with scanf
     mov rdi, scanf_format ; Address of scanf format string
     lea rsi, [rsp + 4] ; Address to store n
     lea rdx, [rsp] ; Address to store r
     call scanf
-    
+
     ; Validate the inputs
-    cmp rax, 2 ; Checking the return value of scanf (that shows number of matchings)
+    cmp rax, 2 ; Check the return value of scanf
     jl invalid
-    xor rcx, rcx ; I want to save the result in rcx so we should reset it
-    jmp function ; Go to the function
+    xor rcx, rcx ; Reset rcx to store the result
+    mov rax, [rsp + 4] ; Load n
+    mov rbx, [rsp] ; Load r
+    push rax
+    push rbx
+    call function ; Go to the function
+    pop rbx
+    pop rax
+
 print:
-    mov rdi, printf_format 
-    mov rsi, rcx ; Ready to print
-end:    
+    mov rdi, printf_format ; Ready to print
+    mov rsi, rcx
+end:
     call printf
     add rsp, 8
     mov rax, 0 ; The non-error exit code
@@ -33,35 +40,43 @@ invalid:
 function:
     mov rax, [rsp + 4] ; Load n
     mov rbx, [rsp] ; Load r
-    cmp rax, rbx ; n ? r
-    jne function_normal
-    je equal ; C(n,n) = 1
-    jl less ; C(n,r>n) = 0
-    cmp rbx, 0 ; C(n,r<0) = 0
-back:
-    jmp print
-
-function_normal:
+    cmp rbx, 0
+    jl zero ; C(n, r < 0) = 0
+    cmp rax, rbx
+    jl less ; C(n, r > n) = 0
+    je equal ; C(n, n) = 1
+    ; Recursive case: C(n, r) = C(n - 1, r) + C(n - 1, r - 1)
     dec rax ; n - 1
     push rax
     push rbx
-    jmp function
-    pop rbx
-    dec rbx ; r - 1
-    push rbx 
-    jmp function
+    call recurse ; Compute C(n - 1, r)
     pop rbx
     pop rax
-    jmp back
+
+    dec rbx ; r - 1
+    push rax
+    push rbx
+    call recurse ; Compute C(n - 1, r - 1)
+    pop rbx
+    pop rax
+
+back:
+    ret
+
+recurse:
+    call function
+    ret
 
 equal:
-    inc rcx ; C(n,n) = 1
+    inc rcx ; C(n, n) = 1
     jmp back
+
 less:
-    ; C(n,r>n) = 0
+    ; C(n, r > n) = 0
     jmp back
+
 zero:
-    ; C(n,r<0) = 0
+    ; C(n, r < 0) = 0
     jmp back
 
 
